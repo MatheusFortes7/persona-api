4e4fd7b7972476704e97e99d80c182bf89edbfc2
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2022-01-06 13:17:21 -0300

    add complete aplication

---
+++ personaapi/src/main/java/com/bootcamp/personapi/dto/mapper/PersonMapper.java
@@ -0,0 +1,15 @@
+package com.bootcamp.personapi.dto.mapper;
+
+import com.bootcamp.personapi.dto.request.PersonDTO;
+import com.bootcamp.personapi.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PersonMapper {
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO dto);
+
+    PersonDTO toDTO(Person dto);
+}
--- personapi/src/main/java/com/bootcamp/personapi/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.personapi.repository;
-
-import com.bootcamp.personapi.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
